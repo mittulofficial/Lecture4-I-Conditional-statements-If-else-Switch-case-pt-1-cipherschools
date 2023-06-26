d6b6da4e8e17eb176a1dac4d26b4ca69ac915680
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-06-26 16:16:58 +0530

    Create fourcondition.cpp
    taking 4 variables here

---
+++ fourcondition.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  Lecture4 I: Conditional statements
+//
+//  Created by MITTUL KUMAR on 26/06/23.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    int c,d;
+    cout<<"input 4 integes a,b,c and d "<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    // statement:a>b
+    cout<<(a>b && c>d);
+    
+}
