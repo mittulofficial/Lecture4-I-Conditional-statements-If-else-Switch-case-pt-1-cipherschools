a1e816244ee61cace41d09a941d90525f7f11333
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-06-26 16:12:38 +0530

    Create fitstconditional.cpp
    a>b

---
+++ fitstconditional.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Lecture4 I: Conditional statements
+//
+//  Created by MITTUL KUMAR on 26/06/23.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"input 2 integes a and b"<<endl;
+    cin>>a;
+    cin>>b;
+    // statement:a>b
+    cout<<(a>b);
+    
+}
